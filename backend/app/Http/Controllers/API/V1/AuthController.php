cd9331c48c83d174e7561943c5f63c5afa2b37ae
Randy <randeepf7@gmail.com>
2024-07-03 20:41:31 +0530

    init auth branch

---
+++ backend/app/Http/Controllers/API/V1/AuthController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers\API\V1;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignUpRequest;
+use Illuminate\Http\Request;
+
+class AuthController extends Controller
+{
+    public function login(LoginRequest $request)
+    {
+        
+    }
+
+    public function register(SignUpRequest $request)
+    {
+        
+    }
+
+    public function logout()
+    {
+        
+    }
+}
